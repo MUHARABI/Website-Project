d39e84b570b38dbad96c22f3590c0500a8a5fb34
MUHARABI <muhammedharabi10@gmail.com>
2024-07-11 12:16:06 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,24 +1,24 @@
 function openSection(evt, sectionName) {
     var i, tabcontent, tablinks;
 
-    // Hide all tab content by default
+    
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
 
-    // Remove the active class from all tabs
+    
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
 
-    // Show the current tab and add an active class to the button that opened it
+    
     document.getElementById(sectionName).style.display = "block";
     evt.currentTarget.className += " active";
 }
 
-// Default to displaying the first tab
+
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementsByClassName("tablinks")[0].click();
 });
